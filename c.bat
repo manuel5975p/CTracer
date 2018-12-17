93183963a5e9e8cf0d6edf9dee2541b19c772b6d
manuel5975p <manuel.winkler@gmx.ch>
2018-12-17 16:40:47 +0100

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\mingw-w64\x86_64-7.2.0-win32-seh-rt_v5-rev1\mingw64\bin\g++.exe" tracer.cpp bmp.cpp vec3.cpp -std=c++17 -O3 -ffast-math -march=native
+"C:\Program Files\mingw-w64\x86_64-7.2.0-win32-seh-rt_v5-rev1\mingw64\bin\g++.exe" tracer.cpp bmp.cpp vec3.cpp -std=c++17 -static -O3 -ffast-math -march=native
